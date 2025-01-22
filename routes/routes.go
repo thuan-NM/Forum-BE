70d71d5a7b12be4a9f6564bd042f8556ab6f12c3
ThuanNguyenITVN <nguyenminhthuan2003st@gmail.com>
2025-01-22 09:57:30 +0700

    Update Database

--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	"Forum_BE/config"
+	//"Forum_BE/config"
 	"Forum_BE/middlewares"
 	"Forum_BE/models"
 	"Forum_BE/repositories"
@@ -17,7 +17,7 @@ func SetupRoutes(r *gin.Engine, db *gorm.DB, jwtSecret string) {
 	permService := services.NewPermissionService(permissionRepo, userRepo)
 
 	var permissions []models.Permission
-	config.InitPermissions(&permissions)
+	//config.InitPermissions(&permissions)
 
 	for _, perm := range permissions {
 		existingPerm, err := permService.GetPermission(string(perm.Role), perm.Resource, perm.Action)
